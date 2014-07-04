661f20a2211dbad28f1068930809459c6e9c23e8
Justin Armstrong <justinmeister@gmail.com>
2014-07-04 10:49:50 -0700

    Began implementing accessign the NPR api

--- app/src/main/java/com/collywobble/justin/nprarticleretriever/HeadlineListFragment.java
+++ app/src/main/java/com/collywobble/justin/nprarticleretriever/HeadlineListFragment.java
@@ -1,14 +1,20 @@
 package com.collywobble.justin.nprarticleretriever;
 
 import android.app.ListFragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class HeadlineListFragment extends ListFragment {
+    private String mNprUrl = "http://api.npr.org/query?apiKey=";
+    private String key = "MDE1MTExMTM3MDE0MDQ0OTEwMDQyNzAyYQ001";
     private ArrayList<String> mHeadlines;
     private ArrayAdapter<String> mAdapter;
+    private static final String TAG = "HeadlineListFragment";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,22 @@ public class HeadlineListFragment extends ListFragment {
                                             android.R.layout.simple_list_item_1,
                                             mHeadlines);
         setListAdapter(mAdapter);
+        new FetchItemsTask().execute();
+
+    }
 
 
+    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+
+                String result = new NprRequest().getUrl("http://www.google.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
     }
 }
