3bc1e8bc5a300c0a04864eeaf6631ed0e857f7b8
Justin Armstrong <justinmeister@gmail.com>
2014-07-05 21:07:32 -0700

    Latest headlines now load from NPR.org, did some minor formating to make it look pretty.

--- app/src/main/java/com/collywobble/justin/nprarticleretriever/HeadlineListFragment.java
+++ app/src/main/java/com/collywobble/justin/nprarticleretriever/HeadlineListFragment.java
@@ -4,52 +4,98 @@ import android.app.ListFragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class HeadlineListFragment extends ListFragment {
-    private String mNprUrl = "http://api.npr.org/query?apiKey=";
-    private String mKey = "MDE1MTExMTM3MDE0MDQ0OTEwMDQyNzAyYQ001";
-    private String mNumOfResults = "&numResults=10";
-    private String mFormat = "&format=json";
-    private String mTopicId = "&id=1007";
-    private String mRequestUrl = mNprUrl + mKey + mNumOfResults + mFormat + mTopicId;
-    private ArrayList<String> mHeadlines;
-    private ArrayAdapter<String> mAdapter;
+
+    private String mRequestUrl = makeRequestUrl(20, 1007);
+    private ArrayList<String> mHeadlines = new ArrayList<String>();
     private static final String TAG = "HeadlineListFragment";
-    private ArrayList<String> mHeadlineStrings;
     private HeadlineDatabase mHeadlineDatabase = new HeadlineDatabase(getActivity());
+    private static final String DATA = "data";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.headline_title);
-        mHeadlines = mHeadlineDatabase.getHeadlineList();
-        mAdapter = new ArrayAdapter<String>(getActivity(),
-                                            android.R.layout.simple_list_item_1,
-                                            mHeadlines);
-        setListAdapter(mAdapter);
-        new FetchItemsTask().execute();
+        if (savedInstanceState == null) {
+            new FetchItemsTask().execute();
+        } else {
+            mHeadlines = savedInstanceState.getStringArrayList(DATA);
+            setupAdapter();
+        }
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        if (mHeadlines != null) {
+            savedInstanceState.putStringArrayList(DATA, mHeadlines);
+        }
+    }
+
+    private String makeRequestUrl(int numOfItems, int topicIdint) {
+        String nprUrl = "http://api.npr.org/query?apiKey=";
+        String key = "MDE1MTExMTM3MDE0MDQ0OTEwMDQyNzAyYQ001";
+        String numOfResults = "&numResults=" + numOfItems;
+        String format = "&format=json";
+        String topicId = "&id=" + topicIdint;
+
+        return nprUrl + key + numOfResults + format + topicId;
+    }
+
+    private void setupAdapter() {
+        ArrayAdapter<String> adapter = new HeadlineAdapter(mHeadlines);
+        setListAdapter(adapter);
     }
 
 
-    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+    private class FetchItemsTask extends AsyncTask<Void,Void,String> {
         @Override
-        protected Void doInBackground(Void... params) {
+        protected String doInBackground(Void... params) {
             try {
-                String result = new NprRequest().getUrl(mRequestUrl);
-                mHeadlineDatabase.setRawStringData(result);
+                return new NprRequest().getUrl(mRequestUrl);
 
             } catch (IOException ioe) {
                 Log.e(TAG, "Failed to fetch URL: ", ioe);
             }
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(String rawStringData) {
+            mHeadlineDatabase.setRawStringData(rawStringData);
+            mHeadlines = mHeadlineDatabase.getHeadlineStringArray();
+            setupAdapter();
+        }
+    }
+
+    private class HeadlineAdapter extends ArrayAdapter<String> {
+        public HeadlineAdapter(ArrayList<String> headlines) {
+            super(getActivity(), 0, headlines);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.list_item_headline, null);
+            }
+
+            String currentHeadline = getItem(position);
+
+            TextView headlineTextView =
+                    (TextView)convertView.findViewById(R.id.headline_text_view);
+            headlineTextView.setText(currentHeadline);
+
+            return convertView;
         }
     }
 }
